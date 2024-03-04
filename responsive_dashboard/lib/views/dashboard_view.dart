9cbbc2172946f88773a70050da82dd1a1d690adc
HamzaLahmedi <hamzalahmedi07@gmail.com>
2024-03-04 18:16:31 +0100

    add assets images as svgs

---
+++ responsive_dashboard/lib/views/dashboard_view.dart
@@ -0,0 +1,20 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/views/widgets/adptive_layout.dart';
+import 'package:responsive_dashboard/views/widgets/desktop_layout.dart';
+
+class DashboardView extends StatelessWidget {
+  const DashboardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (context) => const SizedBox(),
+        tabletLayout: (context) => const SizedBox(),
+        desktopLayout: (context) => const DesktopLayout(),
+      ),
+    );
+  }
+}
