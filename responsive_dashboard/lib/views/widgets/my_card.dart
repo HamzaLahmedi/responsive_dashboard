7d1d42554049ae308365820bbf9459c7b23e13c4
HamzaLahmedi <hamzalahmedi07@gmail.com>
2024-03-12 13:59:36 +0100

    card ui done

--- responsive_dashboard/lib/views/widgets/my_card.dart
+++ responsive_dashboard/lib/views/widgets/my_card.dart
@@ -21,8 +21,11 @@ class MyCard extends StatelessWidget {
           ),
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
+              contentPadding:
+                  const EdgeInsets.only(left: 31, right: 42, top: 16),
               title: Text(
                 'Name card',
                 style: AppStyles.styleRegular16.copyWith(
@@ -32,6 +35,30 @@ class MyCard extends StatelessWidget {
               subtitle:
                   const Text('Sayeh Bandi', style: AppStyles.styleMedium20),
               trailing: SvgPicture.asset(Assets.imagesGallery),
+            ),
+            const Expanded(child: SizedBox()),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    '0918 8124 0042 8129',
+                    style: AppStyles.styleSemiBold24.copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    '12/20 - 124',
+                    style: AppStyles.styleRegular16.copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 54 - 28,
             )
           ],
         ),
